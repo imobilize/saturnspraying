70bdf31283ec09730e8410096830ea53684058ea
Andre Barrett <andre@imobilize.co.uk>
2024-08-30 23:41:57 +0100

    Added klaro cookie consent directly to webiste

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Mukta', ...defaultTheme.fontFamily.sans],
+        sans: [...defaultTheme.fontFamily.sans],
         poppins: ['"Poppins"', 'cursive'],
       },
       container: {
